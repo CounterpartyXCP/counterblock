3398572f7fa284c27bc382752f88a0882d26c299
JahPowerBit <lightzarlboro@gmail.com>
2014-07-05 22:02:22 +0200

    don't record rpsresolves and bet_match_resolutions in transaction_stats

--- lib/blockfeed.py
+++ lib/blockfeed.py
@@ -305,7 +305,7 @@ def process_cpd_blockfeed(zmq_publisher_eventfeed):
                 if msg['command'] == 'insert' \
                    and msg['category'] not in ["debits", "credits", "order_matches", "bet_matches",
                        "order_expirations", "bet_expirations", "order_match_expirations", "bet_match_expirations",
-                       "rps_matches", "rps_expirations", "rps_match_expirations"]:
+                       "rps_matches", "rps_expirations", "rps_match_expirations", "rpsresolves", "bet_match_resolutions"]:
                     mongo_db.transaction_stats.insert({
                         'block_index': cur_block_index,
                         'block_time': cur_block['block_time_obj'],
