cb8250cba528d816652339fdca976bc3add5b9d0
JahPowerBit <lightzarlboro@gmail.com>
2014-11-26 12:04:29 +0000

    update testnet first block

--- counterblockd.py
+++ counterblockd.py
@@ -408,7 +408,7 @@ if __name__ == '__main__':
 
     #More testnet
     if config.TESTNET:
-        config.BLOCK_FIRST = 154908
+        config.BLOCK_FIRST = 281000
     else:
         config.BLOCK_FIRST = 278270
 
